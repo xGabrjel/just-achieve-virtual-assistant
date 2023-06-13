2b936b57616b72c4f0e01710e18d26a641c61e71
xGabrjel <gabriel.luczyszyn@o2.pl>
2023-06-13 19:08:07 +0200

    Task #1: First configuration of gradle and project structure

--- src/test/java/com/appliaction/justAchieveVirtualAssistant/JustAchieveVirtualAssistantApplicationTests.java
+++ src/test/java/com/appliaction/justAchieveVirtualAssistant/JustAchieveVirtualAssistantApplicationTests.java
@@ -5,9 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JustAchieveVirtualAssistantApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        JustAchieveVirtualAssistantApplication.main(new String[]{});
+    }
 }
