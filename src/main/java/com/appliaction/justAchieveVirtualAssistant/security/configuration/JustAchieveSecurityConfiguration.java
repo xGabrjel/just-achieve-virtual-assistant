6fba45e4b3cce3b7cd560ea42187e521161fdc99
xGabrjel <gabriel.luczyszyn@o2.pl>
2023-07-03 15:28:50 +0200

    Task #26 : Changed default security configuration to lambda dsl configuration (non-deprecated)

--- src/main/java/com/appliaction/justAchieveVirtualAssistant/security/configuration/JustAchieveSecurityConfiguration.java
+++ src/main/java/com/appliaction/justAchieveVirtualAssistant/security/configuration/JustAchieveSecurityConfiguration.java
@@ -1,12 +1,14 @@
 package com.appliaction.justAchieveVirtualAssistant.security.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,48 +20,53 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class JustAchieveSecurityConfiguration {
 
     private final JustAchieveSecurityUserDetailsService justAchieveSecurityUserDetailsService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider(){
+    public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(justAchieveSecurityUserDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
     }
+
     @Bean
+    @ConditionalOnProperty(value = "spring.security.enabled", havingValue = "true", matchIfMissing = true)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers(
-                        "/",
-//                        "/justAchieveVirtualAssistant",
-                        "/error",
-                        "/users",
-                        "/defaultError",
-                        "/login",
-                        "/registration/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .usernameParameter("email")
-                .defaultSuccessUrl("/")
-                .permitAll()
-                .and()
-                .logout()
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/")
-                .and()
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request ->
+                        request
+                                .requestMatchers("/", "/error", "/users", "/defaultError", "/login", "/registration/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .usernameParameter("email")
+                                .defaultSuccessUrl("/")
+                                .permitAll()
+                )
+                .logout(logout ->
+                        logout
+                                .invalidateHttpSession(true)
+                                .clearAuthentication(true)
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .logoutSuccessUrl("/")
+                )
+                .build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "spring.security.enabled", havingValue = "false")
+    SecurityFilterChain securityDisabled(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll())
                 .build();
     }
 }
