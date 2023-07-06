f9928ceb1f2604ed7e7493942b829d9ac2331e5d
xGabrjel <gabriel.luczyszyn@o2.pl>
2023-07-06 23:17:01 +0200

    Task #32 : Added some more WebMvcTests

--- src/test/java/com/appliaction/justAchieveVirtualAssistant/security/user/UserControllerTest.java
+++ src/test/java/com/appliaction/justAchieveVirtualAssistant/security/user/UserControllerTest.java
@@ -1,6 +1,8 @@
 package com.appliaction.justAchieveVirtualAssistant.security.user;
 
+import com.appliaction.justAchieveVirtualAssistant.api.dto.UserDTO;
 import com.appliaction.justAchieveVirtualAssistant.api.dto.mapper.UserMapper;
+import com.appliaction.justAchieveVirtualAssistant.domain.User;
 import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 
 @WebMvcTest(UserController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -29,5 +35,56 @@ class UserControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.model().attributeExists("users"))
                 .andExpect(MockMvcResultMatchers.view().name("users"));
+    }
+
+    @Test
+    void showUpdateFormWorksCorrectly() throws Exception {
+        //given
+        int userId = 1;
+
+        User user = new User();
+        user.setUserId(1);
+        user.setUsername("test");
+        user.setEmail("test@o2.pl");
+        user.setActive(true);
+
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUserId(1);
+        userDTO.setUsername("test");
+        userDTO.setEmail("test@o2.pl");
+        userDTO.setActive(true);
+
+        when(userService.findById(userDTO.getUserId())).thenReturn(Optional.of(user));
+        when(userMapper.map(user)).thenReturn(userDTO);
+
+        //when, then
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/edit/{userId}", userId))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().attributeExists("user"))
+                .andExpect(MockMvcResultMatchers.model().attribute("user", userDTO))
+                .andExpect(MockMvcResultMatchers.view().name("update-user"));
+    }
+
+    @Test
+    void updateUserWorksCorrectly() throws Exception {
+        //given
+        int userId = 1;
+
+        //when, then
+        mockMvc.perform(MockMvcRequestBuilders.post("/users/update/{userId}", userId))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/users?success"));
+    }
+
+    @Test
+    void deleteUserWorksCorrectly() throws Exception {
+        //given
+        int userId = 1;
+
+        //when, then
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/delete/{userId}", userId))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/users?delete_success"));
     }
 }
