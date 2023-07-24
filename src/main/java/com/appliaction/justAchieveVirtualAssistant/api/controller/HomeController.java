81a16467bf04ca689268285590159fc3521f10d7
xGabrjel <gabriel.luczyszyn@o2.pl>
2023-07-24 16:07:56 +0200

    Task #63 : Added home page feature - hidden navigation until user profile registration and profile update

--- src/main/java/com/appliaction/justAchieveVirtualAssistant/api/controller/HomeController.java
+++ src/main/java/com/appliaction/justAchieveVirtualAssistant/api/controller/HomeController.java
@@ -1,15 +1,42 @@
 package com.appliaction.justAchieveVirtualAssistant.api.controller;
 
+import com.appliaction.justAchieveVirtualAssistant.business.UserProfileService;
+import com.appliaction.justAchieveVirtualAssistant.domain.UserProfile;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class HomeController {
 
+    private final UserProfileService userProfileService;
+
     @GetMapping
-    public String homePage() {
+    public String homePage(
+            Model model,
+            Principal principal
+    ) {
+
+        boolean isProfileCompleted;
+
+        try {
+            String username = principal.getName();
+            UserProfile userProfile = userProfileService.findByUsername(username);
+
+            isProfileCompleted = userProfile != null;
+
+        } catch (Exception e) {
+            isProfileCompleted = false;
+        }
+
+        model.addAttribute("isProfileCompleted", isProfileCompleted);
         return "home";
     }
 
--- src/test/java/com/appliaction/justAchieveVirtualAssistant/api/controller/HomeControllerMvcTest.java
+++ src/test/java/com/appliaction/justAchieveVirtualAssistant/api/controller/HomeControllerMvcTest.java
@@ -1,13 +1,21 @@
 package com.appliaction.justAchieveVirtualAssistant.api.controller;
 
+import com.appliaction.justAchieveVirtualAssistant.business.UserProfileService;
+import com.appliaction.justAchieveVirtualAssistant.domain.UserProfile;
 import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.security.Principal;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(HomeController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -16,26 +24,56 @@ class HomeControllerMvcTest {
 
     private final MockMvc mockMvc;
 
+    @MockBean
+    private UserProfileService userProfileService;
+
     @Test
+    @WithMockUser
     void homePageWorksCorrectly() throws Exception {
-        //given, when, then
-        mockMvc.perform(MockMvcRequestBuilders.get("/"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("home"));
+        //given
+        String username = "testuser";
+        Principal principal = () -> username;
+        UserProfile completedProfile = new UserProfile();
+
+        when(userProfileService.findByUsername(username))
+                .thenReturn(completedProfile);
+
+        // when, then
+        mockMvc.perform(get("/")
+                .principal(principal))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(model().attribute("isProfileCompleted", true));
+    }
+
+    @Test
+    void homePageWorksCorrectlyWithEmptyProfile() throws Exception {
+        //given
+        String username = "testuser";
+        UserProfile emptyProfile = null;
+
+        when(userProfileService.findByUsername(username))
+                .thenReturn(emptyProfile);
+
+        // when, then
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(model().attribute("isProfileCompleted", false));
     }
 
     @Test
     void loginPageWorksCorrectly() throws Exception {
         //given, when, then
-        mockMvc.perform(MockMvcRequestBuilders.get("/login"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("login"));
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
     }
     @Test
     void invalidAndExpiredTokenErrorWorksCorrectly() throws Exception {
         //given, when, then
-        mockMvc.perform(MockMvcRequestBuilders.get("/error"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("error"));
+        mockMvc.perform(get("/error"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("error"));
     }
 }
