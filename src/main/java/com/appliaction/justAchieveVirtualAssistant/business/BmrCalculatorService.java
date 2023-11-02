2e8f639d2b712afad34946a436e9f6f9a788d242
xGabrjel <gabriel.luczyszyn@o2.pl>
2023-11-02 16:37:55 +0100

    Task #70 : Some more refactoring in bmr calculator

--- src/main/java/com/appliaction/justAchieveVirtualAssistant/business/BmrCalculatorService.java
+++ src/main/java/com/appliaction/justAchieveVirtualAssistant/business/BmrCalculatorService.java
@@ -1,15 +1,14 @@
 package com.appliaction.justAchieveVirtualAssistant.business;
 
 import com.appliaction.justAchieveVirtualAssistant.business.support.ActivityLevel;
+import com.appliaction.justAchieveVirtualAssistant.domain.UserProfile;
 import com.appliaction.justAchieveVirtualAssistant.infrastructure.database.repository.UserProfileRepository;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-@Slf4j
 @Service
 @AllArgsConstructor
 public class BmrCalculatorService {
@@ -20,50 +19,58 @@ public class BmrCalculatorService {
         BigDecimal bmr = calculateActivityExcludedBMR(username);
         BigDecimal armMultiplier = activityLevel.getArmMultiplier();
 
-        BigDecimal result = bmr.multiply(armMultiplier).setScale(2, RoundingMode.HALF_UP);
-
-        log.info("BMR including activity level is equal to: [%s] kcal".formatted(result));
-        return result;
+        return bmr.multiply(armMultiplier).setScale(2, RoundingMode.HALF_UP);
     }
 
     public BigDecimal calculateActivityExcludedBMR(String username) {
-        BigDecimal weight = userProfileRepository.findByUserUsername(username).getWeight();
-        BigDecimal height = userProfileRepository.findByUserUsername(username).getHeight();
-        Integer age = userProfileRepository.findByUserUsername(username).getAge();
-        String sex = userProfileRepository.findByUserUsername(username).getSex();
+        UserProfile userProfile = userProfileRepository.findByUserUsername(username);
+        BigDecimal weight = userProfile.getWeight();
+        BigDecimal height = userProfile.getHeight();
+        Integer age = userProfile.getAge();
+        String sex = userProfile.getSex();
 
-        if (sex.equalsIgnoreCase("MALE")) {
+        if (isMale(sex)) {
             return calculateByRevisedHarrisBenedictFormulaForMale(weight, height, age);
         } else {
             return calculateByRevisedHarrisBenedictFormulaForFemale(weight, height, age);
         }
     }
 
-    public BigDecimal calculateByRevisedHarrisBenedictFormulaForFemale(BigDecimal weight, BigDecimal height, Integer age) {
+    private boolean isMale(String sex) {
+        return sex != null && sex.equalsIgnoreCase("MALE");
+    }
 
+    public BigDecimal calculateByRevisedHarrisBenedictFormulaForFemale(BigDecimal weight, BigDecimal height, Integer age) {
         final BigDecimal femaleWeightFactor = BigDecimal.valueOf(9.25);
         final BigDecimal femaleHeightFactor = BigDecimal.valueOf(310);
         final BigDecimal femaleAgeFactor = BigDecimal.valueOf(4.33);
         final BigDecimal femaleAdditiveFactor = BigDecimal.valueOf(447.6);
 
-        return (((femaleAdditiveFactor
-                .add(femaleWeightFactor.multiply(weight)))
-                .add(femaleHeightFactor.multiply(height)))
-                .subtract(femaleAgeFactor.multiply(BigDecimal.valueOf(age))))
+        BigDecimal weightContribution = femaleWeightFactor.multiply(weight);
+        BigDecimal heightContribution = femaleHeightFactor.multiply(height);
+        BigDecimal ageContribution = femaleAgeFactor.multiply(BigDecimal.valueOf(age));
+
+        return femaleAdditiveFactor
+                .add(weightContribution)
+                .add(heightContribution)
+                .subtract(ageContribution)
                 .setScale(2, RoundingMode.HALF_UP);
     }
 
     public BigDecimal calculateByRevisedHarrisBenedictFormulaForMale(BigDecimal weight, BigDecimal height, Integer age) {
-
         final BigDecimal maleWeightFactor = BigDecimal.valueOf(13.4);
         final BigDecimal maleHeightFactor = BigDecimal.valueOf(480);
         final BigDecimal maleAgeFactor = BigDecimal.valueOf(5.68);
         final BigDecimal maleAdditiveFactor = BigDecimal.valueOf(88.4);
 
-        return (((maleAdditiveFactor
-                .add(maleWeightFactor.multiply(weight)))
-                .add(maleHeightFactor.multiply(height)))
-                .subtract(maleAgeFactor.multiply(BigDecimal.valueOf(age))))
+        BigDecimal weightContribution = maleWeightFactor.multiply(weight);
+        BigDecimal heightContribution = maleHeightFactor.multiply(height);
+        BigDecimal ageContribution = maleAgeFactor.multiply(BigDecimal.valueOf(age));
+
+        return maleAdditiveFactor
+                .add(weightContribution)
+                .add(heightContribution)
+                .subtract(ageContribution)
                 .setScale(2, RoundingMode.HALF_UP);
     }
 }
